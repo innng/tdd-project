4672b690593ff43dfa5f9548e7dbde692c269438
Ingrid Rosselis <ingridrosselis@gmail.com>
2020-09-12 18:43:28 -0300

    Melhoria 2: Removendo Sleeps para Evitar Não Determinismo

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,7 +2,10 @@ import time
 
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
+
+MAX_WAIT = 5
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -43,11 +46,10 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id("id_new_item")
         inputbox.send_keys("Estudar testes de unidade")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # A página atualiza novamente, e agora mostra ambos os itens na sua lista
-        self.check_for_row_in_list_table("1: Estudar testes funcionais")
-        self.check_for_row_in_list_table("2: Estudar testes de unidade")
+        self.wait_for_row_in_list_table("1: Estudar testes funcionais")
+        self.wait_for_row_in_list_table("2: Estudar testes de unidade")
 
         # Maria se pergunta se o site vai lembrar da sua lista. Então, ela verifica que
         # o site gerou uma URL única para ela -- existe uma explicação sobre essa feature
@@ -56,7 +58,15 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Satisfeita, ela vai dormir
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id("id_list_table")
+                rows = table.find_elements_by_tag_name("tr")
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
